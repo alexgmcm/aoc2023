f3d368de7c93b5bea81e32d7d46a040382198168
Alexander McMurray <alexgmcm@gmail.com>
2023-12-07 17:06:38 +0100

    day 1 done

--- public/js/1.js
+++ public/js/1.js
@@ -1,8 +1,10 @@
+//list = list.slice(0,100)
+//list = ["6six73fivetwonjbbxkccdgmlbpjoneightnlj"]
 let arr = []
 //let nums = []
-let firstNum;
-let lastNum;
 let sums = [];
+let sums2 = [];
+let pairs = [];
 const getStyle = (c,curNums) => {
     if(!isNaN(parseInt(c, 10))){
         if (curNums.length===0){
@@ -22,8 +24,29 @@ for (let i=0; i<list.length; i++){
 arr.push( () => {
 return new Promise(resolve => {
 setTimeout(function(list){
+    let firstNum
+    let lastNum
     let curNums = []
+    let numMap = {"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
     let text = list[i]
+    var re = /(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|([1-9])/gi
+    var m, res = [];
+
+    while (m = re.exec(text)) {
+        res.push(m[0]);
+        re.lastIndex = m.index + 1; // <- Important
+    }
+    console.log(res);
+    let matches = res
+    let firstNum2 = !isNaN(parseInt(matches[0], 10)) ? matches[0] : numMap[matches[0]]
+    let lastNum2 = !isNaN(parseInt(matches[matches.length-1], 10)) ? matches[matches.length-1] : numMap[matches[matches.length-1]]  
+    sums2.push(parseInt(firstNum2.toString()+lastNum2.toString(),10))
+    pairs.push([firstNum2,lastNum2])
+    document.getElementById("debug").innerHTML += text
+    document.getElementById("debug").innerHTML += '   -   ' + firstNum2 +'   -   ' + lastNum2 
+    document.getElementById("debug").innerHTML += "  - " + parseInt(firstNum2.toString()+lastNum2.toString(),10) + "<br>"
+
+
     let newText = ""
     for(var j=0; j<text.length; j++)
         {
@@ -32,9 +55,14 @@ setTimeout(function(list){
         }
     document.getElementById("p1").innerHTML = newText;
     lastNum = curNums.pop()
-    let sum = parseInt(firstNum + lastNum.toString(),10)
+    let sum;
+    if (typeof firstNum === 'undefined'){
+        sum = 0
+    } else {
+    sum = parseInt(firstNum + lastNum.toString(),10)
+    }
     sums.push(sum)
-    document.getElementById("p1").innerHTML += '<span style="color:red; font-weight:bold">' + sum + "</span>"
+    document.getElementById("p1").innerHTML += '   -   <span style="color:red; font-weight:bold">' + sum + "</span>"
     resolve(true)
 }
 , 1,list);
@@ -46,7 +74,11 @@ setTimeout(function(list){
 arr.push(() => {
     let sum = sums.reduce((a,b)=>a+b)
     let text = '<span style="color:red; font-weight:bold">' + sum + "</span>"
-    document.getElementById("total").innerHTML = text;    
+    document.getElementById("total").innerHTML = text;
+    console.log(sums2.length)
+    document.getElementById("sums").innerHTML = "Part 2 Total: " + sums2.reduce((a,b)=> a+b)   
+    //document.getElementById("sums").innerHTML += "<br> Part 2 series: " +sums2
+    //document.getElementById("sums").innerHTML += "<br> Part 2 pairs: " +pairs
 })
 
 async function run_funcs(arr){
@@ -55,3 +87,5 @@ async function run_funcs(arr){
   }
 
   run_funcs(arr)
+
+// 6six73fivetwonjbbxkccdgmlbpjoneightnlj 
